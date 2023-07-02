f635b75c9fe1de14f7b377703303d4aa0e7b5392
ric.nader@gmail.com <ric.nader@gmail.com>
2023-07-02 18:09:48 -0300

    Deterninants and eigenValues used together example finished

--- Simple2X2DeterminantsCalc.py
+++ Simple2X2DeterminantsCalc.py
@@ -7,25 +7,18 @@
 # then
 # |X| = ad-bc
 
-# |det(X)| quantifies volume change as a result of applying X:
-# - If det(X) = 0, then X collapses space completely in at least
-#   one dimension, thereby elimination all volume. In this case
-#   we'll be dealing with a singular matrix (with paralel or infinite results)
-#
-# - If 0 < |det(X)| < 1 then X contracts volume to some extent  
-# - If |det(X)| = 1 then X preserves volume exactly
-# - If |det(X)| > 1 then X expands volume
 
 import numpy as np
 
-print("- Defining Matrix")
+print("- Defining matrix X")
 X = np.array([[4,2],[-5,-3]])
 print(X)
 print("")
 
 print("- Calculating the determinant of X")
-det = np.linalg.det(X)
-print(det)
+detX = np.linalg.det(X)
+print(detX)
+print("")
 
 
 
