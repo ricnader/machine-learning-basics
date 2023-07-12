9f6282b424feef70d002c0cb1d047cb190aae456
ric.nader@gmail.com <ric.nader@gmail.com>
2023-07-12 14:17:51 -0300

    Fixing problem where pyPlots were not being shown

--- tensor-operations/PlotCalc.py
+++ tensor-operations/PlotCalc.py
@@ -1,11 +1,11 @@
-# Not graphically Functional yet 
+# Not graphically Functional yet
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 print("- Defining x")
-x =np.linspace(-10,10,1000)
+x = np.linspace(-10, 10, 1000)
 print("")
 
 
@@ -15,16 +15,18 @@ print("")
 
 
 print("- Defining y2")
-y2 = 1 + (5*x) /2
+y2 = 1 + (5*x) / 2
 print("")
 
 print("- Setting up the plot")
 fig, ax = plt.subplots()
 plt.xlabel("x")
 plt.ylabel("y")
-ax.set_xlim([0,3])
-ax.set_ylim([0,8])
-ax.plot(x,y1,c="green")
-ax.plot(x,y2,c="brown")
+ax.set_xlim([0, 3])
+ax.set_ylim([0, 8])
+ax.plot(x, y1, c="green")
+ax.plot(x, y2, c="brown")
 plt.axvline(x=2, color="purple", linestyle='--')
-_=plt.axhline(y=6,color="purple",linestyle ='--')
+_ = plt.axhline(y=6, color="purple", linestyle='--')
+
+plt.show()
