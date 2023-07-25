a7182e15ab396ca5a6ded503407e6ee24af28045
ric.nader@gmail.com <ric.nader@gmail.com>
2023-07-25 17:21:23 -0300

    Implementation of derivatives file

--- calculus/DerivativesFromLimits.py
+++ calculus/DerivativesFromLimits.py
@@ -17,16 +17,48 @@ print("Defining x as start, finish and n points")
 x = np.linspace(-10, 10, 10000) 
 print("")
 
-print("Defining y")
-y = x**2 + 2*x + 2
+# print("Defining y")
+# y = x**2 + 2*x + 2
+# print("")
+
+print("Creating function for finding the y\n"
+      "given an x and an y-intercept")
+def f(my_x):
+    my_y = my_x**2 + 2*my_x + 2
+    return my_y
 print("")
 
-print("For some kind of simple functions we can take a result empirically \n"+
-      "So, trying to get close infinitelly of the unknow")
-def my_fxn(my_x):
-    my_y = (my_x**2 - 1)/(my_x - 1)
-    return my_y
+print("Defining y")
+y = f(x)
+print(y)
+print("")
 
-y = my_fxn(x)
+#According to the explanation on top of this file
+#lets create our function to calculate the derivarives
+print("Creating function that calcs the derivatives")
 
+def diff_demo(my_f, my_x, my_delta):
+    return (my_f(my_x + my_delta) - my_f(my_x)) / my_delta
+print("")
+
+print("Creating array to simulate some deltas")
+
+deltas = [1, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001]
+print("")
+
+
+print("Iterating through deltas, calculating derivatives")
+print("Pay attention to how close the value becames to y")
+print("through each calculation")
+print("x equals to 2")
+
+for delta in deltas:
+    print(diff_demo(f, 2, delta))
+    print("")
+    
+print("x equals to -1")    
+
+for delta in deltas:
+    print(diff_demo(f, -1, delta))
+print("")
 # plt.show();
